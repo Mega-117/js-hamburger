f47648302887a4e33a255a5fe44f008fa3863411
Mega-117 <enricosaian@hotmail.it>
2021-10-11 15:17:55 +0200

    apparire hamburger menu

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let menu = document.querySelector('div.hamburger-menu');
+let menuIco = document.querySelector('div.header-right>a');
+menuIco.addEventListener('click',
+    function () {
+        menu.style.display = "block";
+    }
+)
+let closeMenu = document.querySelector('div.hamburger-menu a.close');
+
+
